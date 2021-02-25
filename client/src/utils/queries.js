a03e5d6c826b660b4da7e5c95edd10e38bc071a3
unknown <melissikiss@gmail.com>
2021-02-24 19:22:23 -0800

    added profile page and login auth

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -35,7 +35,6 @@ export const QUERY_THOUGHT = gql`
     }
   }
 `;
-
 export const QUERY_USER = gql`
   query user($username: String!) {
     user(username: $username) {
@@ -52,6 +51,46 @@ export const QUERY_USER = gql`
         thoughtText
         createdAt
         reactionCount
+      }
+    }
+  }
+`;
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
       }
     }
   }
